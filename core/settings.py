f31d0e5ba674c597f1b41e257705a76c4d3daac1
劉家銘 <ji394python@gmail.com>
2022-05-16 00:59:52 +0800

    datta

--- core/settings.py
+++ core/settings.py
@@ -21,7 +21,7 @@ DEBUG = config('DEBUG', default=False)
 
 # load production server from .env
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', config(
-    'SERVER', default='127.0.0.1'), 'denver0515-django.herokuapp.com/']
+    'SERVER', default='127.0.0.1'), 'denver0515-django.herokuapp.com']
 
 # Application definition
 
