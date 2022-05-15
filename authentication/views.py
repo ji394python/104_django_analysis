269a54aa8dc4dd9509c073f5f3d04d9f0b31f894
劉家銘 <ji394python@gmail.com>
2022-05-16 01:16:53 +0800

    test

--- authentication/views.py
+++ authentication/views.py
@@ -14,6 +14,7 @@ from django.forms.utils import ErrorList
 from django.http import HttpResponse
 from .forms import LoginForm, SignUpForm
 
+
 def login_view(request):
     form = LoginForm(request.POST or None)
 
@@ -28,16 +29,17 @@ def login_view(request):
             if user is not None:
                 login(request, user)
                 return redirect("/")
-            else:    
-                msg = 'Invalid credentials'    
+            else:
+                msg = 'Invalid credentials'
         else:
-            msg = 'Error validating the form'    
+            msg = 'Error validating the form'
 
-    return render(request, "accounts/login.html", {"form": form, "msg" : msg})
+    return render(request, "accounts/login.html", {"form": form, "msg": msg})
+
 
 def register_user(request):
 
-    msg     = None
+    msg = None
     success = False
 
     if request.method == "POST":
@@ -47,15 +49,16 @@ def register_user(request):
             username = form.cleaned_data.get("username")
             raw_password = form.cleaned_data.get("password1")
             user = authenticate(username=username, password=raw_password)
+            print(user, raw_password)
 
-            msg     = 'User created.'
+            msg = 'User created.'
             success = True
-            
-            #return redirect("/login/")
+
+            # return redirect("/login/")
 
         else:
-            msg = 'Form is not valid'    
+            msg = 'Form is not valid'
     else:
         form = SignUpForm()
 
-    return render(request, "accounts/register.html", {"form": form, "msg" : msg, "success" : success })
+    return render(request, "accounts/register.html", {"form": form, "msg": msg, "success": success})
