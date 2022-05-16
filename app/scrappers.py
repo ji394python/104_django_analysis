bb98d39933413f23aee48ff2fedfd422f97f3478
劉家銘 <ji394python@gmail.com>
2022-05-16 14:59:47 +0800

    test crawler

--- app/scrappers.py
+++ app/scrappers.py
@@ -4,13 +4,12 @@ import requests
 from datetime import datetime
 
 
-
 # KLOOK客路網站
 class Klook():
-    
-    def __init__(self,city_name):
+
+    def __init__(self, city_name):
         self.city_name = city_name
-        
+
     def scrape(self):
 
         result = []  # 回傳結果
@@ -20,6 +19,7 @@ class Klook():
             # 取得傳入城市的所有一日遊&導賞團票券
             response = requests.get(
                 f"https://www.ptt.cc/bbs/Stock/index.html")
+            print(response.status_code, response.text)
             soup = BeautifulSoup(response.text, "lxml")
 
             # 取得十個票券卡片(Card)元素
@@ -31,32 +31,32 @@ class Klook():
                 try:
                     title = activity.select('.title > a')[0].text
                 except:
-                    title= "測試"
+                    title = "測試"
 
                 # 票券詳細內容連結
                 try:
                     link = "https://www.ptt.cc/" + \
                         activity.select('.title > a')[0]['href']
                 except:
-                    link= "測試"
+                    link = "測試"
 
                 # 票券價格
                 try:
                     price = activity.select('.nrec')[0].text
                 except:
-                    price= "測試"
+                    price = "測試"
 
                 # 最早可使用日期
                 try:
                     booking_date = activity.select('.meta > .date')[0].text
                 except:
-                    booking_date= "測試"
+                    booking_date = "測試"
 
                 # 評價
                 try:
                     star = activity.select('.meta > .author')[0].text
                 except:
-                    star= "測試"
+                    star = "測試"
 
                 result.append(
                     dict(title=title, link=link, price=price, booking_date=booking_date, star=star, source="https://cdn.klook.com/s/dist_web/assert/desktop/imgs/favicon-098cf2db20.png"))
